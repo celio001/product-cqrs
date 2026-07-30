d4c0bf23f6d612ea4d1e4410efd2fe58b6ee2fb0
celio0001 <pessoalcelio01@gmail.com>
2026-07-30 09:13:55 -0300

    tests(brands): refactor tests create brands

--- product-command/internal/brands/service/service_test.go
+++ product-command/internal/brands/service/service_test.go
@@ -14,41 +14,55 @@ import (
 	"go.uber.org/mock/gomock"
 )
 
-func TestCreateBrands_Success(t *testing.T) {
-	ctx := context.Background()
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
-	mockBrandsRepo := brands_mocks.NewMockBrandsRepoInterface(ctrl)
-	uuid := uuid.New()
-
-	brand := brands.Brand{Name: "Marca-teste"}
-
-	mockBrandsRepo.EXPECT().CreateBrand(ctx, brand).Return(brands.Brand{ID: uuid, Name: "Marca-teste", CreatedAt: time.Now()}, nil)
-
-	svc := NewBrandSvc(mockBrandsRepo)
-	brand, err := svc.CreateBrandSvc(ctx, brand)
-
-	assert.NoError(t, err)
-	assert.IsType(t, brand, brands.Brand{})
-	assert.Equal(t, brand.Name, brand.Name)
-}
-
-func TestCreateBrands_Error(t *testing.T) {
+func TestCreateBrands(t *testing.T) {
 	logger.Init("product-command", "1.0.0", "development")
-	ctx := context.Background()
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
+	tests := []struct {
+		name             string
+		brand            brands.Brand
+		ctx              context.Context
+		mockBrandsReturn brands.Brand
+		mockError        error
+		expected         brands.Brand
+		expectedError    bool
+	}{
+		{
+			name:             "Success",
+			brand:            brands.Brand{Name: "Marca-teste"},
+			ctx:              context.Background(),
+			mockBrandsReturn: brands.Brand{ID: uuid.New(), Name: "Marca-teste", CreatedAt: time.Now()},
+			mockError:        nil,
+			expected:         brands.Brand{ID: uuid.New(), Name: "Marca-teste", CreatedAt: time.Now()},
+			expectedError:    false,
+		}, {
+			name:             "Error",
+			brand:            brands.Brand{Name: "Marca-teste"},
+			ctx:              context.Background(),
+			mockBrandsReturn: brands.Brand{},
+			mockError:        errors.New("error create brand"),
+			expected:         brands.Brand{},
+			expectedError:    true,
+		},
+	}
+	for _, tt := range tests {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
 
-	mockBrandsRepo := brands_mocks.NewMockBrandsRepoInterface(ctrl)
+		mockRepo := brands_mocks.NewMockBrandsRepoInterface(ctrl)
+		mockRepo.EXPECT().
+			CreateBrand(tt.ctx ,tt.brand).
+			Return(tt.mockBrandsReturn, tt.mockError).
+			Times(1)
+		
+		svc := NewBrandSvc(mockRepo)
+		brand, err := svc.CreateBrandSvc(tt.ctx, tt.brand)
 
-	brand := brands.Brand{Name: "Marca-teste"}
-
-	mockBrandsRepo.EXPECT().CreateBrand(ctx, brand).Return(brands.Brand{}, errors.New("error create bands in database"))
-
-	svc := NewBrandSvc(mockBrandsRepo)
-	brand, err := svc.CreateBrandSvc(ctx, brand)
-
-	assert.Error(t, err)
-	assert.Equal(t, brands.Brand{}, brand)
+		if tt.expectedError {
+			assert.Error(t, err)
+			assert.Equal(t, brands.Brand{}, brand)
+		} else if !tt.expectedError{
+			assert.NoError(t, err)
+			assert.IsType(t, brand, brands.Brand{})
+			assert.Equal(t, brand.Name, brand.Name)
+		}
+	}
 }
