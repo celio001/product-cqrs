c60ebda4e0f16281d871c28b8587ed2f43c88371
Célio Máximo Vieira Junior <celio@192.168.15.28>
2026-08-02 20:46:50 -0300

    feat: config server and setup to http server

---
+++ product-command/internal/fiber/server.go
@@ -0,0 +1,37 @@
+package fiber
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/celio001/product-command/config"
+	v1 "github.com/celio001/product-command/internal/fiber/v1"
+	"github.com/celio001/product-command/internal/middleware"
+	f "github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/healthcheck"
+)
+
+func (h HttpServer) Start(ctx context.Context) error {
+
+	h.app.Use(middleware.LoggerMiddleware())
+	api := h.app.Group("/api")
+
+	h.app.Get(healthcheck.LivenessEndpoint, healthcheck.New())
+
+	h.app.Get("/health", healthcheck.New(healthcheck.Config{
+		ResponseFormat: healthcheck.FormatJSON,
+	}))
+
+	v1Router := api.Group(v1.HandlerPath)
+	v1.RegisterRouter(v1Router, h.brandsSvc)
+
+	addr := fmt.Sprint(":", config.GetString("HTTP_PORT"))
+	
+	return h.app.Listen(addr, f.ListenConfig{
+		DisableStartupMessage: true,
+	})
+}
+
+func (h HttpServer) Stop(ctx context.Context) error {
+	return h.app.Shutdown()
+}
---
+++ product-command/internal/fiber/setup.go
@@ -0,0 +1,24 @@
+package fiber
+
+import (
+	brandsSvc "github.com/celio001/product-command/internal/brands/service"
+	"github.com/gofiber/fiber/v3"
+)
+
+type HttpServer struct {
+	app       *fiber.App
+	brandsSvc brandsSvc.BrandSvcInterface
+}
+
+func CreateApp(brandsSvc brandsSvc.BrandSvcInterface) HttpServer {
+	app := fiber.New(fiber.Config{
+		StrictRouting:         true,
+	})
+
+	httpServer := HttpServer{
+		app:       app,
+		brandsSvc: brandsSvc,
+	}
+
+	return httpServer
+}
