323f93d4836e64428ba8393c6c5f271ff539ad3d
celio001 <pessoalcelio01@gmail.com>
2026-08-04 10:34:43 -0300

    feat(validate): add create categories validate

--- product-command/pkg/validate/validate.go
+++ product-command/pkg/validate/validate.go
@@ -18,3 +18,20 @@ func BrandsValidateError(err error) map[string]string {
 
 	return errors
 }
+
+func CategoriesValidateError(err error) map[string]string {
+	errors := make(map[string]string)
+
+	if validationErrs, ok := err.(validator.ValidationErrors); ok {
+		for _, e := range validationErrs {
+			f := e.Field()
+
+			switch f {
+			case "Name":
+				errors[f] = "name must be between 3 and 50 characters."
+			}
+		}
+	}
+
+	return errors
+}
