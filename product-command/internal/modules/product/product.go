3af798c45f4c365a1e50b89801f1c0fe043bd714
celio001 <pessoalcelio01@gmail.com>
2026-08-08 12:41:53 -0300

    feat(product): add model product

---
+++ product-command/internal/modules/product/product.go
@@ -0,0 +1,30 @@
+package product
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Product struct {
+	ID                  uuid.UUID
+	BrandID             uuid.UUID
+	CategoryID          uuid.UUID
+	Name                string
+	Sku                 string
+	BarCodeEan          string
+	ShortDescription    string
+	DetailedDescription string
+	UnitOfMeasure       string
+	CostPrice           float64
+	SalePrice           float64
+	PromotionalPrice    float64
+	GrossWeifht         float64
+	NetWeight           float64
+	Height              float64
+	Width               float64
+	Length              float32
+	Status              string
+	CreatedAt           time.Time
+	UpdateAt            time.Time
+}
