54c503f7ec3bbb8a90d6886b5b8451759ec65378
celio001 <pessoalcelio01@gmail.com>
2026-08-17 22:19:10 -0300

    feat(repository): refactor categories repository to use transaction queries

--- product-command/internal/modules/categories/repository/repository.go
+++ product-command/internal/modules/categories/repository/repository.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 
+	"github.com/celio001/product-command/internal/database"
 	"github.com/celio001/product-command/internal/modules/categories"
 	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5"
@@ -16,16 +17,34 @@ var (
 
 type categoriesRepo struct {
 	PgPool *pgxpool.Pool
+	Tx     *database.Queries
 }
 
 type CategoriesInterface interface {
 	GetCategoryByID(ctx context.Context, id uuid.UUID) (categories.Categories, error)
 	CreateCategory(ctx context.Context, categorie categories.Categories) (categories.Categories, error)
 	SoftDeleteCategory(ctx context.Context, id uuid.UUID) error
+
+	WithTx(tx pgx.Tx) CategoriesInterface
+	BeginTx(context.Context) (pgx.Tx, error)
 }
 
-func NewCategoriesRepo(PgPool *pgxpool.Pool) CategoriesInterface {
-	return &categoriesRepo{PgPool: PgPool}
+func NewCategoriesRepo(PgPool *pgxpool.Pool, tx *database.Queries) CategoriesInterface {
+	return &categoriesRepo{
+		PgPool: PgPool,
+		Tx:     tx,
+	}
+}
+
+func (r *categoriesRepo) WithTx(tx pgx.Tx) CategoriesInterface {
+	return &categoriesRepo{
+		PgPool: r.PgPool,
+		Tx:     r.Tx.WithTx(tx),
+	}
+}
+
+func (r *categoriesRepo) BeginTx(ctx context.Context) (pgx.Tx, error) {
+	return r.PgPool.Begin(ctx)
 }
 
 func (c *categoriesRepo) GetCategoryByID(ctx context.Context, id uuid.UUID) (categories.Categories, error) {
@@ -33,7 +52,7 @@ func (c *categoriesRepo) GetCategoryByID(ctx context.Context, id uuid.UUID) (cat
 	var category categories.Categories
 
 	query := "SELECT id, name FROM categories WHERE id = $1 AND deleted_at IS NULL"
-	err := c.PgPool.QueryRow(ctx, query, id).Scan(&category.ID, &category.Name)
+	err := c.Tx.DB.QueryRow(ctx, query, id).Scan(&category.ID, &category.Name)
 	if err != nil {
 		if errors.Is(err, pgx.ErrNoRows) {
 			return categories.Categories{}, ErrCategoryNotFound
@@ -47,7 +66,7 @@ func (c *categoriesRepo) GetCategoryByID(ctx context.Context, id uuid.UUID) (cat
 
 func (c *categoriesRepo) CreateCategory(ctx context.Context, categorie categories.Categories) (categories.Categories, error) {
 	query := "INSERT INTO categories(name) VALUES($1) RETURNING id, parent_id"
-	err := c.PgPool.QueryRow(ctx, query, categorie.Name).Scan(&categorie.ID, &categorie.ParentID)
+	err := c.Tx.DB.QueryRow(ctx, query, categorie.Name).Scan(&categorie.ID, &categorie.ParentID)
 	if err != nil {
 		return categories.Categories{}, err
 	}
@@ -57,7 +76,7 @@ func (c *categoriesRepo) CreateCategory(ctx context.Context, categorie categorie
 
 func (c *categoriesRepo) SoftDeleteCategory(ctx context.Context, id uuid.UUID) error {
 	query := "UPDATE categories SET deleted_at = now(), updated_at = now() WHERE id = $1 AND deleted_at IS NULL"
-	result, err := c.PgPool.Exec(ctx, query, id)
+	result, err := c.Tx.DB.Exec(ctx, query, id)
 	if err != nil {
 		return err
 	}
