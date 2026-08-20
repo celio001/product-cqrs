f93fbf84fd24571a2473bde0f23f3fe625a66b81
celio001 <pessoalcelio01@gmail.com>
2026-08-19 21:16:35 -0300

    feat(producer): add PublishCategoryCreated method to handle category creation events

--- product-command/internal/modules/producer/producer.go
+++ product-command/internal/modules/producer/producer.go
@@ -8,6 +8,7 @@ import (
 
 	product_dto "github.com/celio001/product-command/internal/fiber/v1/product/dto"
 	"github.com/celio001/product-command/internal/modules/brands"
+	"github.com/celio001/product-command/internal/modules/categories"
 	"github.com/celio001/product-command/pkg/logger"
 	"github.com/google/uuid"
 	"github.com/segmentio/kafka-go"
@@ -22,6 +23,7 @@ type producerCommand struct {
 type ProducerCommandInterface interface {
 	PublishProductCreated(ctx context.Context, p product_dto.CreateProductResponse) error
 	PublishBrandCreated(ctx context.Context, b brands.Brand) error
+	PublishCategoryCreated(ctx context.Context, c categories.Categories) error
 }
 
 func NewProducerCommand(ProductTopic *kafka.Writer, BrandTopic *kafka.Writer) ProducerCommandInterface {
@@ -79,6 +81,33 @@ func (k *producerCommand) PublishBrandCreated(ctx context.Context, b brands.Bran
 		logger.Error("failed to publish the message brand created",
 			zap.String("error.message", err.Error()),
 			zap.String("error.code", "ERROR_PUBLISH_CREATE_PRODUCT"),
+		)
+		return err
+	}
+
+	return nil
+}
+
+func (k *producerCommand) PublishCategoryCreated(ctx context.Context, c categories.Categories) error {
+	valueBytes, err := json.Marshal(c)
+	if err != nil {
+		return fmt.Errorf("falha ao serializar envelope do pedido: %w", err)
+	}
+
+	kafkaHeaders := []kafka.Header{
+		{Key: "event_type", Value: []byte("category.created")},
+		{Key: "trace_id", Value: []byte(uuid.New().String())},
+	}
+
+	err = k.BrandTopic.WriteMessages(ctx, kafka.Message{
+		Value:   valueBytes,
+		Headers: kafkaHeaders,
+		Time:    time.Now(),
+	})
+	if err != nil {
+		logger.Error("failed to publish the message category created",
+			zap.String("error.message", err.Error()),
+			zap.String("error.code", "ERROR_PUBLISH_CREATE_CATEGORY"),
 		)
 		return err
 	}
