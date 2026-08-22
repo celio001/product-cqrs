ada64a43e5830ce9d8bfa9e37e15d3851810fc02
celio001 <pessoalcelio01@gmail.com>
2026-08-22 18:55:29 -0300

    feat(config): add configuration management with environment variable loading

---
+++ worker/config/config.go
@@ -0,0 +1,67 @@
+package config
+
+import (
+	"log"
+	"os"
+	"strings"
+
+	"github.com/joho/godotenv"
+)
+
+type Configs struct {
+	ServiceName    string
+	ServiceVersion string
+	Env            string
+	KafkaCfg       KafkaConfig
+	MongoDB        MongoDB
+}
+
+type KafkaConfig struct {
+	KafkaBrokers   []string
+	ProductTopic   string
+	ProdctDlqTopic string
+	BrandTopic     string
+}
+
+type MongoDB struct {
+	DSN string
+}
+
+func LoadEnvs() *Configs {
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	return &Configs{
+		ServiceName:    os.Getenv("SERVICE_NAME"),
+		ServiceVersion: os.Getenv("SERVICE_VERSION"),
+		Env:            os.Getenv("ENV"),
+		KafkaCfg: KafkaConfig{
+			KafkaBrokers:   kafkaGetStrings("KAFKA_BROKERS"),
+			ProductTopic:   os.Getenv("KAFKA_PRODUCT_TOPIC"),
+			ProdctDlqTopic: os.Getenv("KAFKA_PRODUCT_DLQ_TOPIC"),
+			BrandTopic:     os.Getenv("KAFKA_BRAND_TOPIC"),
+		},
+		MongoDB: MongoDB{
+			DSN: os.Getenv("MONGO_DB_DSN"),
+		},
+	}
+}
+
+func kafkaGetStrings(k string) []string {
+	v := os.Getenv(k)
+	if v == "" {
+		return nil
+	}
+	parts := strings.Split(v, ",")
+	var out []string
+	for _, p := range parts {
+		p = strings.TrimSpace(p)
+		if p != "" {
+			out = append(out, p)
+		}
+	}
+	return out
+}
