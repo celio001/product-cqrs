927b3c13f64e4aa9eff382f3556e86f58ed4251f
celio001 <pessoalcelio01@gmail.com>
2026-08-28 15:37:11 -0300

    chore(configs): add new envs

--- worker/config/config.go
+++ worker/config/config.go
@@ -18,10 +18,13 @@ type Configs struct {
 }
 
 type KafkaConfig struct {
-	KafkaBrokers   []string
-	ProductTopic   string
-	ProdctDlqTopic string
-	BrandTopic     string
+	KafkaBrokers     []string
+	ProductTopic     string
+	ProdctDlqTopic   string
+	BrandTopic       string
+	BrandDqlTopic    string
+	CategoryTopic    string
+	CategoryDlqTopic string
 }
 
 type MongoDB struct {
@@ -44,10 +47,13 @@ func LoadEnvs() *Configs {
 		ServiceVersion: os.Getenv("SERVICE_VERSION"),
 		Env:            os.Getenv("ENV"),
 		KafkaCfg: KafkaConfig{
-			KafkaBrokers:   kafkaGetStrings("KAFKA_BROKERS"),
-			ProductTopic:   os.Getenv("KAFKA_PRODUCT_TOPIC"),
-			ProdctDlqTopic: os.Getenv("KAFKA_PRODUCT_DLQ_TOPIC"),
-			BrandTopic:     os.Getenv("KAFKA_BRAND_TOPIC"),
+			KafkaBrokers:     kafkaGetStrings("KAFKA_BROKERS"),
+			ProductTopic:     os.Getenv("KAFKA_PRODUCT_TOPIC"),
+			ProdctDlqTopic:   os.Getenv("KAFKA_PRODUCT_DLQ_TOPIC"),
+			BrandTopic:       os.Getenv("KAFKA_BRAND_TOPIC"),
+			BrandDqlTopic:    os.Getenv("KAFKA_BRAND_DLQ_TOPIC"),
+			CategoryTopic:    os.Getenv("KAFKA_CATEGORY_TOPIC"),
+			CategoryDlqTopic: os.Getenv("KAFKA_CATEGORY__DLQ_TOPIC"),
 		},
 		MongoDB: MongoDB{
 			DSN: os.Getenv("MONGO_DB_DSN"),
